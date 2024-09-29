aa01ceab4018f8802a41a17e40d827400f510c0a
systemfile36 <84090525+systemfile36@users.noreply.github.com>
2024-09-30 00:07:12 +0900

    기본적인 레이아웃 구성, 다크 모드 토글 버튼 구현, 기타 수정

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,30 +1,67 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import Title from './components/Title.vue'
+
+let isDark = ref(false);
+
+function toggleDarkHandler() {
+  isDark.value = !isDark.value;
+  var html = document.querySelector('html');
+  var mode = isDark.value ? 'dark' : 'light';
+  html.setAttribute('data-bs-theme', mode);
+}
+
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <div class="pb-2">
+        <button class="btn btn-outline-success" @click="toggleDarkHandler">Toggle Dark</button>
+      </div>
+      
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+</nav>
+
+<Title 
+  @toggleDarkMode="toggleDarkHandler"/>
+  
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style scoped lang="scss">
+
 </style>
---
+++ portfolio/src/components/Title.vue
@@ -0,0 +1,55 @@
+<template>
+<section id ="title">
+    <div class="container">
+      <div class="detail row align-items-center">
+        <figure class="col-md-6">
+          <img src="../assets/img/title.png"/>
+        </figure>
+        <div class="text col-md-6">
+          <h2>Hello, <br/>I'm Lojin</h2>
+        </div>
+      </div>
+      <div class="row align-items-center">
+        <button class="btn btn-primary" @click="$emit('toggleDarkMode')">Toggle Dark Theme</button>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+
+
+</script>
+<style scoped lang="scss">
+    #title {
+
+.container {
+  $navbar_height: 56px; // navbar 높이값
+  //height: 100vh;
+  padding: $navbar_height;
+
+  .row {
+    height: 100%;
+    //flex-direction: row-reverse;
+  }
+}
+
+figure {
+  img {
+    width: 100%;
+  }
+  display: grid;
+  place-items: center;
+}
+
+.text {
+  h2 {
+    font-size: 4em;
+  }
+
+  display: grid;
+  place-items: center;
+}
+
+
+}
+</style>
