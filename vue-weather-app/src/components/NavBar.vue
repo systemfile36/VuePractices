def9bb351bfa2de2b8fc8d0a37ae0e83f9fec233
systemfile36 <84090525+systemfile36@users.noreply.github.com>
2024-10-01 21:26:19 +0900

    Font Awesome을 적용, 상단바 레이아웃, 검색 영역 레이아웃 작성 기타 설정 수정

---
+++ vue-weather-app/src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="navbar">
+        <button class="toggle">
+            <i class="fa-solid fa-bars-staggered icon"></i>
+        </button>
+    </nav>
+</template>
+<script setup>
+
+</script>
+<style scoped lang="scss">
+    .navbar {
+        background: #fff;
+        width: 100%;
+        padding: 15px;
+        display: flex;
+        justify-content: end;
+    }
+
+    .toggle {
+        border: none;
+        background: transparent;
+        cursor: pointer;
+
+        .icon {
+            color: #333;
+            font-size: 24px;
+        }
+        
+    }
+
+</style>
---
+++ vue-weather-app/src/components/SearchBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="search-bar">
+        <form>
+            <div class="form-group">
+                <input type="search" placeholder="enter the location"/>
+                <button>
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style lang="scss">
+    .search-bar {
+        padding: 8px 20px;
+        border-radius: 40px;
+        background: #fff;
+        border: 1px solid #ccc;
+        
+        form {
+            .form-group {
+                display: flex;
+                justify-content: center;
+                
+                input {
+                    width: 100%;
+                    border: none;
+                    font-size: 16px;
+                    outline: none;
+                }
+
+                button {
+                    background: transparent;
+                    border: none;
+                    &:hover {
+                        cursor: pointer;
+                        outline: green solid 2px;
+                    }
+                }
+            }
+        }
+    }
+</style>
